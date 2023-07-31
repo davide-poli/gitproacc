75de7d5d028352614b670484e6ce4593a3b1109a
DESKTOP-3O1D8HD\user <davide.poli@sistemiesoluzioni.it>
2023-07-31 10:54:46 +0200

    Scheda residenziale

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
+	//^ Creation and handling of stepper
 	const stepper = new Stepper(document.querySelector('.bs-stepper'), {
-		linear: false,
+		linear: true,
 		animation: true,
 	});
 	const btnsNext = document.querySelectorAll(`.btn.next`);
 	const btnsPrev = document.querySelectorAll(`.btn.prev`);
 
 	btnsNext.forEach(btn => {
-		btn.addEventListener('click', () => {
+		btn.addEventListener('click', e => {
+			e.preventDefault();
 			stepper.next();
 		});
 	});
 
 	btnsPrev.forEach(btn => {
-		btn.addEventListener('click', () => {
+		btn.addEventListener('click', e => {
+			e.preventDefault();
 			stepper.previous();
 		});
 	});
